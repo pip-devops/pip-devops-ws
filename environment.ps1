7d24f1a1eefee2752ee1130036ed596745226e18
Sergey Seroukhov <seroukhov@gmail.com>
2017-09-09 13:56:52 +0300

    Initial commit

---
+++ environment.ps1
@@ -0,0 +1,9 @@
+$global:All = @(
+    "pip-devops-teamcity"
+    "pip-devops-tsdev"
+)
+
+$global:Images = @(
+    "pip-devops-teamcity"
+    "pip-devops-tsdev"
+)
---
+++ workspace.conf.ps1
@@ -0,0 +1,5 @@
+$VersionControl = 'git'
+$VersionControlRepos = @(
+	"https://github.com/pip-devops/pip-devops-teamcityagent.git"
+	"https://github.com/pip-devops/pip-devops-tsdev.git"
+)
