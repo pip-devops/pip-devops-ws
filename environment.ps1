71ad30665147980903af80f56d3de4c76b2a627f
Sergey Seroukhov <seroukhov@gmail.com>
2017-09-10 12:51:59 +0300

    Added pip-devops-node image

--- environment.ps1
+++ environment.ps1
@@ -1,9 +1,11 @@
 $global:All = @(
     "pip-devops-teamcity"
     "pip-devops-tsdev"
+    "pip-devops-node"
 )
 
 $global:Images = @(
     "pip-devops-teamcity"
     "pip-devops-tsdev"
+    "pip-devops-node"
 )
--- workspace.conf.ps1
+++ workspace.conf.ps1
@@ -2,4 +2,5 @@ $VersionControl = 'git'
 $VersionControlRepos = @(
 	"https://github.com/pip-devops/pip-devops-teamcityagent.git"
 	"https://github.com/pip-devops/pip-devops-tsdev.git"
+	"https://github.com/pip-devops/pip-devops-node.git"
 )
