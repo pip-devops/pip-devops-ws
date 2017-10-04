10a3ae752fa9222a4b957712f53fca785fc914af
Sergey Seroukhov <seroukhov@gmail.com>
2017-10-04 16:55:03 +0300

    Added pip-devops-ps

--- environment.ps1
+++ environment.ps1
@@ -2,10 +2,12 @@ $global:All = @(
     "pip-devops-teamcity"
     "pip-devops-tsdev"
     "pip-devops-node"
+    "pip-devops-ps"
 )
 
 $global:Images = @(
     "pip-devops-teamcity"
     "pip-devops-tsdev"
     "pip-devops-node"
+    "pip-devops-ps"
 )
--- workspace.conf.ps1
+++ workspace.conf.ps1
@@ -3,4 +3,5 @@ $VersionControlRepos = @(
 	"https://github.com/pip-devops/pip-devops-teamcityagent.git"
 	"https://github.com/pip-devops/pip-devops-tsdev.git"
 	"https://github.com/pip-devops/pip-devops-node.git"
+	"https://github.com/pip-devops/pip-devops-ps.git"
 )
